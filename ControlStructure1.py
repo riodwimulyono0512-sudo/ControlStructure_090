7686ad3018892cc80dd30260bffdbf80cf000de2
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-09-22 10:54:02 +0700

    first commit

---
+++ ControlStructure1.py
@@ -0,0 +1,12 @@
+performance = int(input("enter student performance: "))
+
+if performance >=90:
+    print("Excellent performance")
+elif performance >= 80:
+    print("Very Good performance")
+elif performance >=70:
+    print("Good performance")
+elif performance >60:
+    print("average performance")
+else:
+    print("poor performance")
